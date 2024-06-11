b27212e0d0af3cca1ece743d7e9b6e4f2169a9a0
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-11 12:54:16 -0400

    error catch

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ let counter = 0;
 // Create a simple function that increments the variable, and then 
 // calls itself recursively.
 function increment(){
-    {
+  try{
         console.log(counter)
         counter++
         increment()
-        
+       
+    }catch(error){
+        // try and catch work in tandem. e in try and catch universal for error (both work)*
+    console.error(error)
+    console.log(counter)
     }
 
 }
-
-increment()
-// e in try and catch universal for error*
 // Surround the initial function call in a try/catch block.
 // Within the catch, log the error and the value of the counter variable.
+increment()
+
+
+
 // As your first task, complete the steps abov
 // 
