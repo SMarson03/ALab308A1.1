f12e0404fe6297d873205707418fc1d5e3762f82
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-13 16:54:48 -0400

    again

--- app.js
+++ app.js
@@ -13,14 +13,27 @@ function increment(){
         // try and catch work in tandem. e in try and catch universal for error (both work)*
     console.error(error)
     console.log(counter)
+    // Within the catch, log the error and the value of the counter variable.
     }
 
 }
 // Surround the initial function call in a try/catch block.
-// Within the catch, log the error and the value of the counter variable.
 increment()
+//8982 was my outcome
 
 
+function flattenArr(arr){
+    const givenArr = arr.flat()
+    try{
+        console.log(givenArr.flat())
+        flattenArr(arr)
+    }catch(error){
+        console.error(error)
+        console.log(error)
+    }
 
-// As your first task, complete the steps abov
-// 
+}
+
+console.log(flattenArr([1],[2],[3]))
+//Team leader Marlie explained flattening array is a higher function taking a 
+//nested array and making it a single array
